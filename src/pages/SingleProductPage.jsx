c34d083dfeffe53293c4fe03d57678345421a0b3
ronin-developer <milan@outlook.at>
2024-03-31 14:00:30 +0200

    second-commit

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import ProductService from '../services/postService';
+import { AiOutlineCheck } from "react-icons/ai";
 
 function SingleProductPage() {
+  const [product, setProduct] = useState({});
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [price, setPrice] = useState(product.price)
+   
+
+  function handleCurrentImage(index) {
+    setCurrentImage(index);
+  }
+
+  // id for single product
+  let { id } = useParams();
+
+  useEffect(() => {
+    setIsLoading(true);
+    ProductService.getSingleProduct(id)
+      .then((res) => {
+        setProduct(res.data);
+        console.log(res.data)
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  }, [id]);
+
   return (
-    <div>SingleProductPage</div>
-  )
+    <div className='container mx-auto'>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className='flex flex-wrap'>
+          <div className='flex flex-col w-80'>
+          <div className='flex rounded-xl'>
+            <img className='w-80 h-80 flex object-cover rounded-xl' src={product.images && product.images[currentImage]} alt={product.title} />
+          </div>
+          <div className='mx-auto flex gap-4 my-5'>
+          {product.images &&
+            product.images.map((el, i) => {
+              return <img className='w-28 h-28 rounded-xl border border-blue-600 object-cover' key={i} src={el} alt={product.title} onClick={() => handleCurrentImage(i)} />;
+            })}
+            </div>
+            </div>
+          <div className='w-1/2 h-80 mx-auto my-auto p-4 border drop-shadow-md border-b-slate-900 rounded-xl'>
+            <h2>{product.title}</h2>
+            <p>Available product in stock: {product.stock}</p>
+            <p>Product price: ${product.price}</p>
+            <button>Add to cart</button>
+            <hr /><br />
+            <p>Product description: {product.description}</p>
+            <p>{product.rating}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default SingleProductPage
+export default SingleProductPage;
--- src/services/postService.js
+++ src/services/postService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 class ProductService {
     static getAllProducts = () => axios.get('/products')
+    static getSingleProduct = (productId) => axios.get(`/products/${productId}`)
 }
 
 export default ProductService;
